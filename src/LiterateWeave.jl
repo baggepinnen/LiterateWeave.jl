7fd4408a0fd81f6b5cde7040b2cf1f93c637c09e
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2020-03-01 06:56:22 +0800

    better docstring

--- src/LiterateWeave.jl
+++ src/LiterateWeave.jl
@@ -2,7 +2,7 @@ module LiterateWeave
 
 using Literate, Weave
 
-export literateweave
+export literateweave, set_chunk_defaults, restore_chunk_defaults
 
 """
     literateweave(source, args...; kwargs...)
@@ -10,9 +10,44 @@ export literateweave
 Converts the source .jl file to markdown using Literate.jl and then weaves this markdown file using Weave.jl. Write your code file as if you were using Literate.jl
 
 Args and kwargs goes to weave(s, args...; kwargs...)
-Try
+
+- Weave docs: http://weavejl.mpastell.com/stable/
+- Literate docs: https://fredrikekre.github.io/Literate.jl/latest/
+- Filter a line: `#src`
+- Comment in code: `##`
+
+Default weave options can be modified, try
 ```julia
-literateweave("myfile.jl", doctype="md2pdf")
+set_chunk_defaults(Dict(
+:dpi        => 96,
+:fig        => true,
+:fig_pos    => nothing,
+:out_width  => nothing,
+:label      => nothing,
+:eval       => true,
+:wrap       => true,
+:fig_env    => nothing,
+:term       => false,
+:hold       => false,
+:cache      => false,
+:include    => true,
+:prompt     => "\njulia> ",
+:results    => "markup",
+:out_height => nothing,
+:tangle     => true,
+:display    => false,
+:fig_width  => 6,
+:engine     => "julia",
+:fig_path   => "figures",
+:line_width => 75,
+:skip       => false,
+:fig_height => 4,
+:echo       => true,
+:fig_cap    => nothing,
+:fig_ext    => nothing))
+```
+Standard usage:
+```julia
 literateweave("myfile.jl", doctype="md2pdf", latex_cmd="lualatex")
 ```
 """
